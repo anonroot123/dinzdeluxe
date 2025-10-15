554b1ae31fa5a73d82283560f7b26d71b7cedd62
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2025-10-15 17:53:56 +0800

    Create config3.js

---
+++ config3.js
@@ -0,0 +1,15 @@
+serverBaseUrl=https://dinzcess.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=SELECT YOUR DEVICE SPECS!
+
+game1=LOW END DELUXE|liblow3.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
+
+game2=MID END DELUXE|libmid3.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
+
+game3=HIGH END DELUXE|libhigh3.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
+
+game4=PRO VARIANT(BETA)|libpro2.so|PRO VARIANT IS NEW MOD MENU VARIANT FOR DINZ HACKS
+
+game5=DEVELOPER TESTING|libbolbol2.so|DONT USE ITS AUTOBAN, ITS FOR DEVELOPER TESTING ONLY FUCKYOU 
