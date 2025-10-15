b37e38a034c554daed8da3c8f6adbdd1dd60652d
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2025-10-15 23:22:42 +0800

    Update config3.jsbahahaba

--- config3.js
+++ config3.js
@@ -4,13 +4,13 @@ gameActivity=com.tencent.tmgp.cod.CODMainActivity
 
 appTitle=SELECT YOUR DEVICE SPECS!
 
-game1=LOW END DELUXE|liblow3.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
+game1=LOW END DELUXE|liblow2.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
 
-game2=MID END DELUXE|libmid3.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
+game2=MID END DELUXE|libmid2.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
 
-game3=HIGH END DELUXE|libhigh3.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
+game3=HIGH END DELUXE|libhigh2.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
 
-game4=PRO VARIANT(BETA)|libpro2.so|PRO VARIANT IS NEW MOD MENU VARIANT FOR DINZ HACKS
+game4=PRO VARIANT(BETA)|libpro.so|PRO VARIANT IS NEW MOD MENU VARIANT FOR DINZ HACKS
 
 game5=DELUXE SKINHACKS|libbolbol2.so|DONT USE ITS AUTOBAN, ITS FOR DEVELOPER TESTING ONLY FUCKYOU 
 
