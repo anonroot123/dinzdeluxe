b2a47515daaee0b613c3548243e374b1d20ec105
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2025-12-11 13:18:20 +0800

    Update lifetime.js

--- lifetime.js
+++ lifetime.js
@@ -2,18 +2,6 @@ serverBaseUrl=https://dinzcess.shop/OnlineMe/
 
 gameActivity=com.tencent.tmgp.cod.CODMainActivity
 
-appTitle=BURAT WALA PANGA 
+appTitle=BOBO MAG UUPDATE AKO APK
 
-game1=LOW END DELUXE|liblow3.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
 
-game2=MID END DELUXE|libmid3.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
-
-game3=HIGH END DELUXE|libhigh3.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
-
-game4=DINZ PRO (NEW UI)|libpro.so|PRO VARIANT IS NEW MOD MENU VARIANT FOR DINZ HACKS
-
-game5=DELUXE SKINHACKS|libbolbol3.so|THIS IS STILL A TEST: CAUSE SOME DEVICE's ARE CRASHING ON SKINHACKS SO TEST IT FIRST IF COMPATIBLE NO CRASH THEN ENJOY IF CRASH THEN MESSAGE ME @Cesssmod
-
-game6=TEST 2|libt.so| TEST VERSION 2 
-
-game7=TINETEST KOPA BOBO|libaaa.so| TANGINAMO
