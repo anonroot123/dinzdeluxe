305dd7538f16d558dd9f99948ccd0b0e6f6288e1
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2025-12-11 17:59:05 +0800

    Update breakeven.js

--- breakeven.js
+++ breakeven.js
@@ -6,9 +6,9 @@ appTitle=LIFETIME 1.6.53
 
 game1=LOW END DELUXE|libl4.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
 
-game2=MID END DELUXE|libm3.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
+game2=MID END DELUXE|libm4.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
 
-game3=HIGH END DELUXE|libh3.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
+game3=HIGH END DELUXE|libh4.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
 
 game4=DINZ PRO (NEW UI)|libpro.so|PRO VARIANT IS NEW MOD MENU VARIANT FOR DINZ HACKS
 
