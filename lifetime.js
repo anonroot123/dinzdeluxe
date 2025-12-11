1b76fa5cb294f21cb780e3e20b5191a166c2117b
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2025-12-11 16:14:53 +0800

    Update lifetime.js

--- lifetime.js
+++ lifetime.js
@@ -3,6 +3,4 @@ serverBaseUrl=https://dinzcess.shop/OnlineMe/
 gameActivity=com.tencent.tmgp.cod.CODMainActivity
 
 appTitle=BOBO MAG UUPDATE AKO APK
-
-
 game1=TINETEST KOPA BOBO|libaaa.so| TANGINAMO
