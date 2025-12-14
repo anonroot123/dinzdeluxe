5edcae09490ae5e9df25bd9c2d32ecb2f46ce3ae
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2025-12-14 15:57:54 +0800

    Add configuration for game variants in marian.js

---
+++ marian.js
@@ -0,0 +1,16 @@
+serverBaseUrl=https://dinzcess.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=POGI DELUXE 1.6.53
+
+game1=LOW END DEVICE|libl.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
+
+game2=MID END DEVICE|libm.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
+
+game3=HIGH END DEVICE|libh.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
+
+game4=DINZ PRO (NEW UI)|libpro.so|PRO VARIANT IS NEW MOD MENU VARIANT FOR DINZ HACKS
+
+game5=DELUXE SKINHACKS|libs.so|THIS IS STILL A TEST: CAUSE SOME DEVICE's ARE CRASHING ON SKINHACKS SO TEST IT FIRST IF COMPATIBLE NO CRASH THEN ENJOY IF CRASH THEN MESSAGE ME @Cesssmod
+
