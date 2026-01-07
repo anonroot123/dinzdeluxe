7e9b59ac4936f9bdac4a55c234067e988d1037f2
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-01-07 18:12:18 +0800

    Update marian.js

--- marian.js
+++ marian.js
@@ -6,5 +6,5 @@ appTitle=POGI DELUXE 1.6.53
 
 game1=SUPREME POGI|libmarianxd.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
 
-
+game2=TEST DONT USE|libmarianxdt.so|TANGINAMO SABING TEST NGA EH
 
