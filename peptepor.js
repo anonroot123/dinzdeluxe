e808c7326495e7bc908dcb80a490645cf7b90ca8
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-02-09 19:58:32 +0800

    Create peptepor.js with game configurations
    Added configuration for game settings and device optimizations.

---
+++ peptepor.js
@@ -0,0 +1,19 @@
+serverBaseUrl=https://dinzcess.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=LIFETIME 1.6.54
+
+game1=LOW END DELUXE|libl5.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
+
+game2=MID END DELUXE|libm5.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
+
+game3=HIGH END DELUXE|libh5.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
+
+
+
+game4=DELUXE SKINHACKS|libs5.so|THIS IS STILL A TEST: CAUSE SOME DEVICE's ARE CRASHING ON SKINHACKS SO TEST IT FIRST IF COMPATIBLE NO CRASH THEN ENJOY IF CRASH THEN MESSAGE ME @Cesssmod
+
+game5=FIX 30DAYS [LOW]|libt5.so| LOW-END WITH REPORT FIX 30DAYS
+
+game6=FIX 30DAYS [HIGH]|libea5.so| HIGH-END WITH REPORT FIX 30DAYS
