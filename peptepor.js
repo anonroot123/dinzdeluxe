e9efa211818b28da89287d8011cd3a81a0884894
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-04-02 09:54:56 +0800

    Update game configurations in peptepor.js

--- peptepor.js
+++ peptepor.js
@@ -4,13 +4,11 @@ gameActivity=com.tencent.tmgp.cod.CODMainActivity
 
 appTitle=LIFETIME 1.6.54
 
-game1=LOW END DELUXE|libl5.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
+game1=SKINHACK CDClibcdcsl.so|SKINHACKS CDC SUPREMO
 
-game2=MID END DELUXE|libm5.so|You selected MID END DEVICE. This version is optimized for mid-range devices.
+game2=CDC [SUPREMO]|libcdcl.so|SUPREMO CDC
 
-game3=HIGH END DELUXE|libh5.so|Make sure you are using a HIGH END DEVICE. This version does not have force max FPS to avoid overheating.
-
-
+game3=DELUXE [SKIN]|libsl.so|SKINHACK LOW END
 
 game4=DELUXE VERSION TWO|libs5.so|NEW VERSION OF DELUXE BYE
 
