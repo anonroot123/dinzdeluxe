f28fafc520532057adf878aba4dd3763981c9f0c
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-04-18 00:58:15 +0800

    Create 55lifetime.js with game configurations
    Added configuration for LIFETIME game versions and activities.

---
+++ 55lifetime.js
@@ -0,0 +1,17 @@
+serverBaseUrl=https://cessdinzdeluxe.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=LIFETIME 1.6.54
+
+game1=SKINHACK CDC|libcdcsl2.so|SKINHACKS CDC SUPREMO
+
+game2=CDC [SUPREMO]|libcdcl2.so|SUPREMO CDC
+
+game3=DELUXE [SKIN]|libsl2.so|SKINHACK LOW END
+
+game4=DELUXE VERSION TWO|libs6.so|NEW VERSION OF DELUXE BYE
+
+game5=FIX 30DAYS [LOW]|libt6.so| LOW-END WITH REPORT FIX 30DAYS
+
+game6=FIX 30DAYS [HIGH]|libea6.so| HIGH-END WITH REPORT FIX 30DAYS
