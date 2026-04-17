ea2a326eb5f033c9e4c976f071f30cf894e35748
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-04-18 00:09:53 +0800

    Add game configurations for DELUXE 1.6.55
    Added game configurations and metadata for DELUXE 1.6.55.

--- 55deluxe.js
+++ 55deluxe.js
@@ -1,1 +1,17 @@
 
+serverBaseUrl=https://cessdinzdeluxe.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=DELUXE 1.6.55
+
+game1=[CDC SUPREMO]|libcdc2.so|You selected CDC. This version is optimized for low-end devices and forcing max FPS [CESS DINZ CASPER].
+game2=[CDC SKIN HACK]|libcdcs2.so|You selected CDC. This version is optimized for low-end devices and forcing max FPS and SKINHACKS [CESS DINZ CASPER].
+
+
+
+game3=DELUXE VERSION TWO|libss2.so|DELUXE VERSION 2
+game4=FIX 30DAYS [LOW]|libtt2.so| LOW-END WITH REPORT FIX 30DAYS
+
+game5=FIX 30DAYS [HIGH]|libeaa2.so| HIGH-END WITH REPORT FIX 30DAYS
+game6=DELUXE [SKINHACK]|libsnl2.so| SKINHACKS DELUXE
