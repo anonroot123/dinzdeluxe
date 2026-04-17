e9eff47331ef7e6b0d93141376715bb258d03a33
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-04-18 01:30:23 +0800

    Add configuration for POGI DELUXE game

---
+++ 55marian.js
@@ -0,0 +1,9 @@
+serverBaseUrl=https://cessdinzdeluxe.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=POGI DELUXE 1.6.55
+
+game1=SUPREME POGI|libmarianxd3.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
+game2=POGI VERSION 2|libmarianxdx3.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
+
