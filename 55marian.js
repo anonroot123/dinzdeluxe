da9557e35b40fedbddaf1c3a55556e8d796e83cf
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-04-19 15:32:28 +0800

    Add game3 entry for SUPREME POGI skin hack

--- 55marian.js
+++ 55marian.js
@@ -6,4 +6,4 @@ appTitle=POGI DELUXE 1.6.55
 
 game1=SUPREME POGI|libmarianxd3.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
 game2=POGI VERSION 2|libmarianxdx3.so|You selected LOW END DEVICE. This version is optimized for low-end devices and forcing max FPS.
-
+game3=SUPREME POGI [SKIN HACK]|libmarianskin.so|SKIN HACK VERSION 
