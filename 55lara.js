62844444914758bcba56ecf22aab48a95e9a65c3
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-05-01 14:18:54 +0800

    Add configuration for LxL game versions

---
+++ 55lara.js
@@ -0,0 +1,9 @@
+serverBaseUrl=https://cessdinzdeluxe.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=LxL 1.6.55
+
+game1=LARANASxLEY|liblxl.so|NORMAL VERSION AND OPTIMIZE
+
+game2=LARANASxLEY [SKIN HACK]|liblxls.so|SKIN HACK VERSION
