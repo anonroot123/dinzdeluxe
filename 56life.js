c3df94492ddc57da73eed9aea748c3a54903ed31
anonroot123 <150358130+anonroot123@users.noreply.github.com>
2026-06-25 19:31:08 +0800

    Create 56life.js with game configuration
    Added configuration for game activities and titles.

---
+++ 56life.js
@@ -0,0 +1,19 @@
+serverBaseUrl=https://cessdinzdeluxe.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=LIFETIME 1.6.55
+
+game1=SKINHACK CDC|libcdcs3.so|SKINHACKS CDC SUPREMO
+
+game2=CDC [SUPREMO]|libcdc3.so|SUPREMO CDC
+
+game3=DELUXE [SKIN]|libsl3.so|SKINHACK LOW END
+
+game4=DELUXE VERSION TWO|libs3.so|NEW VERSION OF DELUXE BYE
+
+game5=FIX 30DAYS [LOW]|libt3.so| LOW-END WITH REPORT FIX 30DAYS
+
+game6=FIX 30DAYS [HIGH]|libea3.so| HIGH-END WITH REPORT FIX 30DAYS
+
+game7=TANGINAMO WAG MO GAGAMITIN TO NAG TETEST AKO|libeggs.so| PAG GINAMIT MOTO PANGIT KANA BOBO KAPA
