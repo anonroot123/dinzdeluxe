1a8c772321bbe0ce5e0e7d8556ce304080dbc04b
CESS DEV <150358130+anonroot123@users.noreply.github.com>
2026-09-04 11:38:37 +0800

    Initialize 57life.js with game settings
    Added configuration for game activities and titles.

---
+++ 57life.js
@@ -0,0 +1,20 @@
+serverBaseUrl=https://cessdinzdeluxe.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=LIFETIME 1.6.57
+
+game1=SKINHACK CDC|libcdcs4.so|SKINHACKS CDC SUPREMO
+
+game2=CDC [SUPREMO]|libcdc4.so|SUPREMO CDC
+
+game3=DELUXE [SKIN]|libsl4.so|SKINHACK LOW END
+
+game4=DELUXE VERSION TWO|libs4.so|NEW VERSION OF DELUXE BYE
+
+game5=FIX 30DAYS [LOW]|libt4.so| LOW-END WITH REPORT FIX 30DAYS
+
+game6=FIX 30DAYS [HIGH]|libea4.so| HIGH-END WITH REPORT FIX 30DAYS
+
+game7=BETA TESTING [LOW]|libeggs.so| POGI KABA?
+game7=BETA TESTING [HIGH]|libeggsh.so| PANG POGI LANG TONG TEST
