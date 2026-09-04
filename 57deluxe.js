2050d3218ae948046dcbfd5e15e846c0e3360d4b
CESS DEV <150358130+anonroot123@users.noreply.github.com>
2026-09-04 12:12:42 +0800

    Update app version to 1.6.57

---
+++ 57deluxe.js
@@ -0,0 +1,17 @@
+serverBaseUrl=https://cessdinzdeluxe.shop/OnlineMe/
+
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=DELUXE 1.6.57
+
+game1=[CDC SUPREMO]|libcdc4.so|You selected CDC. This version is optimized for low-end devices and forcing max FPS [CESS DINZ CASPER].
+game2=[CDC SKIN HACK]|libcdcs4.so|You selected CDC. This version is optimized for low-end devices and forcing max FPS and SKINHACKS [CESS DINZ CASPER].
+
+
+
+game3=DELUXE VERSION TWO|libss4.so|DELUXE VERSION 2
+game4=FIX 30DAYS [LOW]|libtt4.so| LOW-END WITH REPORT FIX 30DAYS
+
+game5=FIX 30DAYS [HIGH]|libeaa4.so| HIGH-END WITH REPORT FIX 30DAYS
+game6=DELUXE [SKINHACK]|libsnl4.so| SKINHACKS DELUXE
+game7=[BETA TEST DONT USE]|libtesttest.so| THIS IS A TEST VERSION MOTHERFUCKERS
