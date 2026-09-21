f19c7f62e0cec293372034fb0ffba58fdbd005f3
CESS DEV <150358130+anonroot123@users.noreply.github.com>
2026-09-21 12:30:33 +0800

    Add int_c variable assignment in 57life.js

--- 57life.js
+++ 57life.js
@@ -18,3 +18,4 @@ game6=FIX 30DAYS [HIGH]|libea4.so| HIGH-END WITH REPORT FIX 30DAYS
 
 game7=BETA TESTING [LOW]|libeggs.so| POGI KABA?
 game7=BETA TESTING [HIGH]|libeggsh.so| PANG POGI LANG TONG TEST
+int_c=intC.so
